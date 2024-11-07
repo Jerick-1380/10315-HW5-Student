b10147976c80506cfe02504eafc8fbaaa129aab8
Jerick Shi <99981445+Jerick-1380@users.noreply.github.com>
2024-11-07 12:52:09 -0500

    Update code.py

--- src/code.py
+++ src/code.py
@@ -10,7 +10,7 @@ def consistent_scale_eigenvectors(V):
 
         Returns V. V is modified in place and also returned.
 
-        Implementation based on code from https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/utils/extmath.py::svd_flip
+        Implementation based on code from https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/utils/extmath.py
     """
     max_abs_cols = np.argmax(np.abs(V), axis=0)
     signs = np.sign(V[max_abs_cols, range(V.shape[1])])
@@ -22,6 +22,8 @@ def pca(X, K):
         X: numpy ndarray of shape (N, M) where each row represents a data point
         K: integer representing the desired number of output dimensions
 
+        Returns a numpy ndarray of shape (N, K)
+
         Don't forget to center your data first
         Use eigenvectors based on np.linalg.eig or np.linalg.svd.
         We suggested using np.linalg.svd because it returns sorted arrays.
@@ -29,8 +31,6 @@ def pca(X, K):
         NOTE: In order to make the autograder happy, you must pass your eigenvectors
         into the consisten_scale_eigenvectors function above before applying them 
         to your data!
-
-        Returns a numpy ndarray of shape (N, K)
     """
     #Student Solution HERE
     return 0.0
@@ -38,9 +38,6 @@ def pca(X, K):
 
 def kmeans(X, K):
     """ Cluster data X into K converged clusters.
-
-        The algorithm should terminate if there are no changes in labels.
-        MAKE SURE TO INITIALIZE CLUSTERS TO BE THE FIRST K POINTS!!!
     
         X: an N-by-M numpy ndarray, where we want to assign each
             of the N data points to a cluster.
@@ -51,18 +48,21 @@ def kmeans(X, K):
             C: a numpy ndarray with shape (K,M), where each row is a cluster center
             z: a numpy ndarray with shape (N,) where the i-th entry is an int from {0..K-1}
                 representing the cluster index for the i-th point in X
+
+        The algorithm should terminate if there are no changes in labels.
+        MAKE SURE TO INITIALIZE CLUSTERS TO BE THE FIRST K POINTS!!!
     """
     #Student Solution HERE
     return 0.0, 0.0
 
 
 def boxcar(x, z, width):
-    """ Return the result of applying the radial basis function kernel 
-        on the two input vectors, given the hyperparameter gamma.
+    """ Return 1 if the Eudclidean distance between input vectors is
+        less than or equal to width/2, and return zero otherwise.
 
         x: Mx1 numpy ndarray
         z: Mx1 numpy ndarray
-        gamma: float value of hyperparameter
+        width: float value of hyperparameter
         
         Returns: float value after appying kernel to x and z
     """
